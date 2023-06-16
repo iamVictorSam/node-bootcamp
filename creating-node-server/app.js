e9a30608eecc9d298484dc2a4e984be642fbecf7
Sam Victor <iamvictorsam@gmail.com>
2023-06-16 05:52:50 +0100

    fix

--- creating-node-server/app.js
+++ creating-node-server/app.js
@@ -70,7 +70,7 @@ const server = http.createServer((req, res) => {
     return req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, (err) => {
+      fs.writeFile('message.txt', message, err => {
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
